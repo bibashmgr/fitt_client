9791e96918a1656c01c88838ecb96c533edcafd5
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-06-23 12:12:09 +0545

    Add: components for starting_screen

---
+++ lib/screens/starting_screen/components/intro.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+// constants
+import 'package:fitt_client/constants/colors.dart';
+
+class Intro extends StatelessWidget {
+  final PageController pageController;
+  const Intro({Key? key, required this.pageController}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      minimum: const EdgeInsets.all(25),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const SizedBox(
+            height: 25,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  elevation: 0.0,
+                  primary: kPrimaryColor,
+                  padding: EdgeInsets.zero,
+                  minimumSize: const Size(40, 40),
+                  tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                ),
+                onPressed: () {
+                  pageController.animateToPage(
+                    0,
+                    duration: const Duration(milliseconds: 250),
+                    curve: Curves.easeIn,
+                  );
+                },
+                child: const Icon(
+                  Icons.arrow_back_rounded,
+                  color: kLightColor,
+                ),
+              ),
+              const SizedBox(
+                height: 200.0,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const <Widget>[
+                  Image(image: AssetImage('assets/icons/wave.png')),
+                  Text(
+                    'Here are some questions to prepare a personalized plan for you.',
+                    style: TextStyle(
+                      fontFamily: 'Outfit',
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: kDarkColor,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 145.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {
+                      pageController.animateToPage(1,
+                          duration: const Duration(milliseconds: 250),
+                          curve: Curves.easeIn);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      elevation: 0.0,
+                      primary: kPrimaryColor,
+                      minimumSize: const Size(300, 50),
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(50),
+                        ),
+                      ),
+                    ),
+                    child: const Text(
+                      "I'm Ready",
+                      style: TextStyle(
+                        fontFamily: 'Outfit',
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.5,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/starting_screen/components/loading.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+// constants
+import 'package:fitt_client/constants/colors.dart';
+
+class Loading extends StatelessWidget {
+  final PageController pageController;
+  const Loading({Key? key, required this.pageController}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      minimum: const EdgeInsets.all(25),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(
+                height: 100,
+              ),
+              const Image(image: AssetImage('assets/images/app_logo.png')),
+              const Text(
+                'Fitt',
+                style: TextStyle(
+                  color: kPrimaryColor,
+                  fontFamily: 'Outfit',
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 150,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  pageController.animateToPage(1,
+                      duration: const Duration(milliseconds: 250),
+                      curve: Curves.easeIn);
+                },
+                style: ElevatedButton.styleFrom(
+                  elevation: 0.0,
+                  primary: kPrimaryColor,
+                  minimumSize: const Size(300, 50),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(50),
+                    ),
+                  ),
+                ),
+                child: const Text(
+                  'Get Started',
+                  style: TextStyle(
+                    fontFamily: 'Outfit',
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.5,
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
