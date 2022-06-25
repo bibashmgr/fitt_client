c2d6b90d78c18f163cac736eac7363fa9341cbd3
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-06-25 18:08:49 +0545

    Update: Starting screen

--- lib/screens/starting_screen/starting.dart
+++ lib/screens/starting_screen/starting.dart
@@ -6,6 +6,8 @@ import 'package:fitt_client/constants/colors.dart';
 // components
 import 'package:fitt_client/screens/starting_screen/components/loading.dart';
 import 'package:fitt_client/screens/starting_screen/components/intro.dart';
+import 'package:fitt_client/screens/starting_screen/components/info.dart';
+import 'package:fitt_client/screens/starting_screen/components/level.dart';
 
 class Starting extends StatelessWidget {
   const Starting({Key? key}) : super(key: key);
@@ -15,24 +17,38 @@ class Starting extends StatelessWidget {
     final PageController pageController = PageController(
       initialPage: 0,
     );
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: kLightColor,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: Scaffold(
-        body: PageView(
-          controller: pageController,
-          physics: const NeverScrollableScrollPhysics(),
-          children: <Widget>[
-            Loading(
-              pageController: pageController,
-            ),
-            Intro(
-              pageController: pageController,
-            )
-          ],
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.unfocus();
+        }
+      },
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          scaffoldBackgroundColor: kLightColor,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: Scaffold(
+          body: PageView(
+            controller: pageController,
+            physics: const NeverScrollableScrollPhysics(),
+            children: <Widget>[
+              Loading(
+                pageController: pageController,
+              ),
+              Intro(
+                pageController: pageController,
+              ),
+              Info(
+                pageController: pageController,
+              ),
+              Level(
+                pageController: pageController,
+              ),
+            ],
+          ),
         ),
       ),
     );
